772859437821f40c8e202daa9b1e5d194be258d0
Aditya <railswithaditya@gmail.com>
2024-02-26 15:04:35 +0530

    required all the gems in the config/environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -1,1 +1,21 @@
 ENV["RACK_ENV"] ||= "development"
+
+require 'rake'
+require 'pry'
+require 'irb'
+require 'sinatra'
+require 'sinatra/flash'
+require 'sinatra/activerecord'
+require 'require_all'
+require 'pony'
+require 'bcrypt'
+require 'active_record'
+require 'sinatra/activerecord/rake'
+require 'rotp'
+require 'rqrcode'
+require 'dotenv/load'
+require 'sidekiq'
+
+Dir.glob('./app/controllers/concerns/*.rb').each { |file| require file }
+require_all 'app'
+Dir.glob('./app/models/*.rb').each { |file| require file }
