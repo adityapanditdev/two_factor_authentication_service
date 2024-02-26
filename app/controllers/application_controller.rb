d0d08ae6b2dc8f35824ca3a40fa7dcfeace687d6
Aditya <railswithaditya@gmail.com>
2024-02-26 14:49:16 +0530

    added session to perform login functionality

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
-# require_relative './concerns/authentication'
 class ApplicationController < Sinatra::Base
+  include Authentication
+
+  enable :sessions
+  register Sinatra::Flash
+
+  set :session_secret, ENV['SESSION_SECRET']
+  set :views, File.expand_path('../views', __dir__)
 end
