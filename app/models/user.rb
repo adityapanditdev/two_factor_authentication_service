7b7b360492c5ed6b53e0627c8b9c95889c4c4568
Aditya <railswithaditya@gmail.com>
2024-02-26 14:38:01 +0530

    added validations for the user table

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,16 @@
 class User < ApplicationRecord
+  # Validations
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :password, presence: true, length: { minimum: 6 }
+  validate :password_complexity
+
+  private
+
+  def password_complexity
+    return if password.blank?
+
+    unless password.match(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\p{P}\p{S}])[A-Za-z\d\p{P}\p{S}]{6,}$/)
+      errors.add :password, 'must include at least one lowercase letter, one uppercase letter, one digit, one special character, and be at least 6 characters long'
+    end
+  end
 end
